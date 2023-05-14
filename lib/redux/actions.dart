bef54a1946b7646fe0a82df67a9bf9e2f30b78ea
unknown <karkisikij2@gmail.com>
2023-05-14 16:34:17 -0700

    Created actions to add, edit, and remove an exercise from exercise list

---
+++ lib/redux/actions.dart
@@ -0,0 +1,27 @@
+import 'package:app_3_redux/model/model.dart';
+
+// when user wants to add a new exercise
+class AddExerciseAction {
+  final Exercise exercise;
+
+  AddExerciseAction(this.exercise);
+}
+
+// when user wants to edit an exercise
+// replaces the exericse to be edited with a new updated exercise
+class EditExerciseAction {
+  final Exercise exerciseToEdit;
+  final Exercise updatedExercise;
+
+  EditExerciseAction(
+    this.exerciseToEdit,
+    this.updatedExercise,
+  );
+}
+
+// when user wants to delete an exercise
+class RemoveExerciseAction {
+  final Exercise exercise;
+
+  RemoveExerciseAction(this.exercise);
+}
