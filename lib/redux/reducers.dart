7c17d9f13ff17f85acf59e8bf5d94edf0f5f3be2
sik1j <karkisikij2@gmail.com>
2023-05-14 16:45:55 -0700

    created state reducer for exerciseList

---
+++ lib/redux/reducers.dart
@@ -0,0 +1,24 @@
+import 'package:app_3_redux/model/model.dart';
+import 'package:app_3_redux/redux/actions.dart';
+
+AppState appStateReducer(AppState state, dynamic action) {
+  return AppState(
+    exerciseList: exerciseListReducer(state.exerciseList, action),
+  );
+}
+
+List<Exercise> exerciseListReducer(
+    List<Exercise> exerciseList, dynamic action) {
+  if (action is AddExerciseAction) {
+    return List.from(exerciseList)..add(action.exercise);
+  }
+  if (action is EditExerciseAction) {
+    return List.from(exerciseList)
+      ..remove(action.exerciseToEdit)
+      ..add(action.updatedExercise);
+  }
+  if (action is RemoveExerciseAction) {
+    return List.from(exerciseList)..remove(action.exercise);
+  }
+  return exerciseList;
+}
