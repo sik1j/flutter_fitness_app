be1021f1e66c30893a438aeb11e6d87df972f26d
sik1j <karkisikij2@gmail.com>
2023-05-20 19:22:30 -0700

    test message + should be the description of the commit

--- lib/pages/RoutinesPage.dart
+++ lib/pages/RoutinesPage.dart
@@ -78,15 +78,153 @@ class ItemListWidget extends StatelessWidget {
     return Column(
       children: _viewModel.routineList
           .map(
-            (e) => ListTile(
-              title: Text(e.name),
-              leading: IconButton(
-                onPressed: () => _viewModel.onRemoveRoutine(e),
-                icon: const Icon(Icons.delete),
+            (routine) => ListTile(
+              title: Text(routine.name),
+              // leading: IconButton(
+              //   onPressed: () => _viewModel.onRemoveRoutine(routine),
+              //   icon: const Icon(Icons.delete),
+              // ),
+              // trailing: Icon(Icons.edit),
+              trailing: IconButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RoutineCreateOrEditPage(
+                      routine: routine,
+                      onEditRoutine: (routineToEdit, updatedRoutine) =>
+                          _viewModel.onEditRoutine(
+                              routineToEdit, updatedRoutine),
+                    ),
+                  ),
+                ),
+                icon: const Icon(Icons.edit),
               ),
             ),
           )
           .toList(),
+    );
+  }
+}
+
+class RoutineCreateOrEditPage extends StatelessWidget {
+  RoutineCreateOrEditPage({
+    super.key,
+    required this.routine,
+    required this.onEditRoutine,
+  }) : _nameController = TextEditingController.fromValue(
+          TextEditingValue(text: routine.name),
+        );
+
+  final Routine routine;
+  final Function(Routine, Routine) onEditRoutine;
+
+  final TextEditingController _nameController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            onEditRoutine(
+              routine,
+              Routine(name: _nameController.text),
+            );
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'EXERCISE NAME',
+                ),
+              ),
+            ),
+            const Text('EXERCISES:'),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Column(
+                children: [
+                  if (routine.exercises != null) ...[
+                    for (int i = 0; i < routine.exercises!.length; i++)
+                      ExerciseListTile(
+                          name: routine.exercises![i].name, index: i),
+                  ],
+                  if (routine.exercises == null) ...[
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('No Exercises Yet'),
+                    )
+                  ]
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ExerciseListTile extends StatelessWidget {
+  const ExerciseListTile({
+    super.key,
+    required this.name,
+    this.reps,
+    this.intensity,
+    required this.index,
+  });
+
+  final String name;
+  final int index;
+  final int? reps;
+  final int? intensity;
+
+  @override
+  Widget build(BuildContext context) {
+    return
+        // 3 columns, with a title and subtitle
+        Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Text('$index.'),
+              Text(name),
+            ],
+          ),
+        ),
+        Column(
+          children: [
+            const Text('Reps'),
+            Text(reps == null ? '-' : '$reps'),
+          ],
+        ),
+        Column(
+          children: [
+            const Text('Int'),
+            Text(intensity == null ? '-' : '$intensity'),
+          ],
+        )
+      ],
     );
   }
 }
